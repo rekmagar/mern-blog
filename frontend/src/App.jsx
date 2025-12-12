95028f46db4d8fd54477bf775c28201f6f773985
rekmagar <rekmagar@gmail.com>
2025-12-12 14:57:44 +0545

    2082-08-26 Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Login from './pages/Login.jsx'
+import Blogs from './pages/Blogs.jsx'
+import Navbar from './components/Navbar.jsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/about', element: <div>About Page</div> },
-  { path: '/signup', element: <div>Signup Page</div> },
-  { path: '/login', element: <div>Login Page</div> },
-  { path: '/blogs', element: <div>Blogs Page</div> },
+  { 
+    path: '/', 
+    element: <><Navbar /><Home /> </>
+  },
+  { 
+    path: '/about', 
+    element: <><Navbar /><About /> </>
+  },
+  { 
+    path: '/signup', 
+    element: <><Navbar /><SignUp /> </> 
+  },
+  { 
+    path: '/login', 
+    element: <><Navbar /><Login /> </>
+  },
+  { 
+    path: '/blogs', 
+    element: <><Navbar /><Blogs /> </>  
+  },
 ])
 
 const App = () => {
   return (
-    <div className='text-red-500'>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Button from '../components/ui/Button'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Button >Click Me </Button>
+    </div>
   )
 }
 
